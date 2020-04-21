52cad0591eae367dd641ce25bf8e6e7df78c8dba
Corentin <corentin.croizat@onbrane.com>
2020-04-21 20:29:37 +0200

    terraform - add load balancer

--- automation/terraform/application/main.tf
+++ automation/terraform/application/main.tf
@@ -42,3 +42,34 @@ resource "aws_security_group" "application" {
     Name = "${var.stage}-application"
   }
 }
+
+# Create a new load balancer
+resource "aws_elb" "application" {
+  name               = "${var.stage}-app-elb"
+  availability_zones = ["eu-west-2a"]
+
+  listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8080/api/healthcheck"
+    interval            = 30
+  }
+
+  instances                   = aws_instance.application.*.id
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "${var.stage}-elb"
+  }
+}
